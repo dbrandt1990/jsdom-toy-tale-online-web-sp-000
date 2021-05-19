e6bd4d586fe054d48ffeffaf3f78837ddf5f062a
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-19 15:30:29 -0600

    commit tree 9bbbfb84bc5794ab7d91b503805f6fc6ea02f615

--- src/index.js
+++ src/index.js
@@ -13,3 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+window.onload = () => {
+  const toyObj = {}
+  fetch("http://localhost:3000/toys", toyObj)
+    .then(resource => resource.json())
+    .then(obj => {
+      obj.forEach((toy) => {
+        let container = document.getElementById("toy-collection")
+        let toyCard = document.createElement("div")
+        toyCard.class = "card"
+        let likeBtn = document.createElement("button")
+        likeBtn.innerText = "Like <3"
+
+        let name = document.createElement("h2")
+        let img = document.createElement("img")
+        let likes = document.createElement("p")
+        name.innerText = toy.name
+        img.src = toy.image
+        likes.innerText = `${toy.likes} likes`
+
+        toyCard.appendChild(name)
+        toyCard.appendChild(img)
+        toyCard.appendChild(likes)
+        toyCard.appendChild(likeBtn)
+
+        container.appendChild(toyCard)
+      })
+    })
+}
